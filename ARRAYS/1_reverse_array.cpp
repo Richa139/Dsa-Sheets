4cc7ae8c3e8847bce4367656100c087ea31fba91
Richa <66673366+Richa139@users.noreply.github.com>
2021-09-19 22:25:06 +0530

    Add files via upload

---
+++ ARRAYS/1_reverse_array.cpp
@@ -0,0 +1,26 @@
+// 1st method
+// tc:O(n);
+// sc:O(n);
+string reverseWord(string str){
+    string ans;
+    int n=str.length();
+    for(int i=n-1;i>=0;i--){
+    ans+=str[i];
+    }
+    return ans;
+}
+
+// 2nd method
+// tc:O(n);
+// sc:O(1);
+string reverseWord(string str){
+    
+   int i=0;
+   int j=str.length()-1;
+   while(i<j){
+       swap(str[i],str[j]);
+       i++;
+       j--;
+   }
+   return str;
+}
