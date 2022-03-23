1577c2fe6811f00a0a9a2334f4c449a338330958
Richa <66673366+Richa139@users.noreply.github.com>
2022-03-23 15:45:58 +0530

    Update 1_reverse_array.cpp

--- ARRAYS/1_reverse_array.cpp
+++ ARRAYS/1_reverse_array.cpp
@@ -24,26 +24,4 @@ string reverseWord(string str){
    }
    return str;
 }
-// program to reverse a array
-#include <iostream>
-using namespace std;
 
-int main() {
-     int t;
-     cin>>t;
-     
-     while(t--)
-     {
-         int n;
-         cin>>n;
-         int arr[n];
-         for(int i = 0; i < n; i++)
-          cin>>arr[i];
-          
-          for(int i = n-1; i >= 0; i--)
-           cout<<arr[i]<<" ";
-           
-           cout<<endl;
-     }
-	return 0;
-}
