a7bd8198f66436203202f310fe2bb8574fad4409
Richa <66673366+Richa139@users.noreply.github.com>
2022-03-24 11:02:08 +0530

    Add files via upload

---
+++ ARRAYS/prac.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<arr[k-1];
+    
+}
