65db932f3742a9c1fb45b1eaa869e92626932c2b
Richa <66673366+Richa139@users.noreply.github.com>
2022-04-01 23:26:37 +0530

    Update 03.Kth_max_min_array.cpp

--- ARRAYS/03.Kth_max_min_array.cpp
+++ ARRAYS/03.Kth_max_min_array.cpp
@@ -32,10 +32,8 @@ int main()
 //2nd method using Heaps(Priority queue)
 class Solution{
     public:
-    // arr : given array
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
-    // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
          priority_queue<int,vector<int>,greater<int>> p;
     for(int i=0;i<=r;i++){
