b74b1a30ff491ba4b979e00073cb26e6049b8e7a
Richa Kochar <66673366+Richa139@users.noreply.github.com>
2022-09-20 12:17:43 +0530

    Create 09_kthsmallestelement_matrix.cpp

---
+++ Matrix/09_kthsmallestelement_matrix.cpp
@@ -0,0 +1,16 @@
+// 1st method 
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+  //Your code here
+  vector<int>v;
+  for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+          v.push_back(mat[i][j]);
+      }
+  }
+  sort(v.begin(),v.end());
+  return v[k-1];
+}
+
+// 2nd method 
+// Heap
